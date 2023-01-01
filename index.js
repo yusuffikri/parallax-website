4de37b1765179a81e27592852f165f74fe0f6a96
yusuffikri <yusuffiks99@gmail.com>
2023-01-01 14:31:00 +0800

    parallax fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let text = document.getElementById("text");
+let parallax__text = document.getElementById("parallax__text");
 let leaf = document.getElementById("leaf");
 let hill1 = document.getElementById("hill1");
 let hill4 = document.getElementById("hill4");
@@ -7,7 +7,7 @@ let hill5 = document.getElementById("hill5");
 window.addEventListener("scroll", () => {
   let value = window.scrollY;
 
-  text.style.marginTop = value * 1.5 + "px";
+  parallax__text.style.marginTop = value * 1.5 + "px";
   leaf.style.top = value * -1.5 + "px";
   leaf.style.left = value * 1.5 + "px";
   hill5.style.left = value * 1.5 + "px";
